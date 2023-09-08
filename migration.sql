fea42a86064cfeadcf8bb41a23241dfc0bb09f14
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-09-08 10:20:21 -0500

    changed id back to int

--- migration.sql
+++ migration.sql
@@ -1,11 +1,11 @@
 USE tasklister_db;
 
 DROP TABLE IF EXISTS tasks;
-DROP TABLE IF EXISTS household;
+DROP TABLE IF EXISTS households;
 
 
 CREATE TABLE households (
-    id LONG NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -14,13 +14,13 @@ CREATE TABLE households (
 );
 
 CREATE TABLE tasks (
-    id LONG NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     description TEXT,
     status BOOLEAN,
     repeatable BOOLEAN,
     category VARCHAR(100),
-    household_id LONG NOT NULL,
+    household_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (household_id) REFERENCES households(id)
 );
