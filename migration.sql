ea25c25fd121c26cd4e256321423dc95e954cbed
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-09-08 10:11:19 -0500

    refactored mysql tables to hold long for id instead of int

--- migration.sql
+++ migration.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS tasks;
 DROP TABLE IF EXISTS households;
 
 CREATE TABLE households (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id LONG NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -13,13 +13,13 @@ CREATE TABLE households (
 );
 
 CREATE TABLE tasks (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id LONG NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     description TEXT,
     status BOOLEAN,
     repeatable BOOLEAN,
     category VARCHAR(100),
-    household_id INT UNSIGNED NOT NULL,
+    household_id LONG NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (household_id) REFERENCES households(id)
 );
