bc7be615e3b51275c58427d267730ea322436ad4
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-09-08 09:13:45 -0500

    added css directory to webapp and added layout.css to that css directory

--- migration.sql
+++ migration.sql
@@ -1,22 +1,27 @@
-USE adlister_db;
+USE tasklister_db;
 
 DROP TABLE IF EXISTS tasks;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS households;
 
-CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+CREATE TABLE households
+(
+    id       INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    username VARCHAR(50)                 NOT NULL UNIQUE,
+    email    VARCHAR(100)                NOT NULL UNIQUE,
+    password VARCHAR(255)                NOT NULL,
+    name     VARCHAR(150),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE tasks (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    description TEXT NOT NULL,
+CREATE TABLE tasks
+(
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(100) NOT NULL,
+    description TEXT,
+    status      BOOLEAN,
+    repeatable  BOOLEAN,
+    category    VARCHAR(100),
+    household_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-        ON DELETE CASCADE
+    FOREIGN KEY (household_id) REFERENCES households (id)
 );
