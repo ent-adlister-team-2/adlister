43f4ec32c5849ef25ec63ca444c39da9d0813142
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-09-09 19:27:42 -0500

    created changeusername servlet and jsp. gave change username functionality to servlet

---
+++ src/main/java/com/codeup/adlister/controllers/ChangeUsernameServlet.java
@@ -0,0 +1,48 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Household;
+import org.mindrot.jbcrypt.BCrypt;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/profile/change-username")
+public class ChangeUsernameServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("household") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        Household household = (Household) request.getSession().getAttribute("household");
+        request.setAttribute("household", household);
+        request.getRequestDispatcher("/WEB-INF/households/change-username.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Household loggedInHousehold = (Household) request.getSession().getAttribute("household");
+
+        String oldUsername = request.getParameter("old-username");
+        String newUsername = request.getParameter("new-username");
+        String password = request.getParameter("password");
+
+        Boolean validUsername = loggedInHousehold.getUsername().equals(oldUsername);
+        Boolean validPassword = BCrypt.checkpw(password, loggedInHousehold.getPassword());
+
+        if(validUsername && validPassword) {
+            loggedInHousehold.setUsername(newUsername);
+            try {
+                DaoFactory.getUsersDao().updateUsername(loggedInHousehold.getId(), newUsername);
+            } catch (SQLException e) {
+                throw new RuntimeException("Unable to change Username, please try again.", e);
+            }
+        } else {
+            response.sendRedirect("/profile/change-username");
+        }
+    }
+}
