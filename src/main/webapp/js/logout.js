48f5602dc49ffaf040787e8e2b29baab369ca98a
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-09-13 09:30:19 -0500

    landing page link in nav bar now

--- src/main/webapp/js/logout.js
+++ src/main/webapp/js/logout.js
@@ -1,10 +1,9 @@
 // LOGOUT NOTIFICATION //
 
-const logoutNotify = document.getElementsByClassName("logout-notify")[0]; // Assuming there's only one element with this class
+const logoutNotify = document.getElementsByClassName("logout-notify")[0]; //[0] is for the only one element with this class
 
 function handleLogout() {
     alert("You've been logged out!");
-    console.log("FIRED");
 }
 
 logoutNotify.addEventListener("click", handleLogout);
